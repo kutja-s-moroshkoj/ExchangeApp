1e35cddaaa3527fd96aeffe3a1b98af915150fc7
Rostislav <ros.vonomatra@gmail.com>
2025-12-02 14:29:06 +0300

    Initial Commit

---
+++ ScreenEx/ScreenExApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenExApp.swift
+//  ScreenEx
+//
+//  Created by Ростислав on 02.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct ScreenExApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
