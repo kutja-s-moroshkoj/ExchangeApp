21b350055a5728402af5f47d1764c726268d6625
Rostislav <ros.vonomatra@gmail.com>
2025-12-03 21:44:44 +0300

    Add data model

--- ScreenEx/Models/ExchangeModel.swift
+++ ScreenEx/Models/ExchangeModel.swift
@@ -1,40 +1,118 @@
-//
-//  ExchangeModel.swift
-//  ScreenEx
-//
-//  Created by Ростислав on 03.12.2025.
-//
-//
-//
-//    let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets")!
-//    var components = URLComponents(url: url, resolvingAgainstBaseURL: true)!
-//    let queryItems: [URLQueryItem] = [
-//        URLQueryItem(name: "vs_currency", value: "usd"),
-//        URLQueryItem(name: "order", value: "market_cap_desc"),
-//        URLQueryItem(name: "per_page", value: "250"),
-//        URLQueryItem(name: "page", value: "1"),
-//        URLQueryItem(name: "sparkline", value: "true"),
-//        URLQueryItem(name: "price_change_percentage", value: "24"),
-//    ]
-//    components.queryItems = components.queryItems.map {$0 + queryItems} ?? queryItems
-//    
-//    var request = URLRequest(url: components.url!)
-//    request.httpMethod = "GET"
-//    request.timeoutInterval = 10
-//    request.allHTTPHeaderFields = [
-//        "accept": "application/json",
-//        "x-cg-demo-api-key" : "CG-akbL7MnYLPmX7CAAnzEaUtyg"
-//    ]
-//    
-//    let (data, _) = try await URLSession.shared.data(for: request)
-//    print(String(decoding: data, as: UTF8.self))
-//
-//
+
+/**
+ let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets")!
+ var components = URLComponents(url: url, resolvingAgainstBaseURL: true)!
+ let queryItems: [URLQueryItem] = [
+ URLQueryItem(name: "vs_currency", value: "usd"),
+ URLQueryItem(name: "order", value: "market_cap_desc"),
+ URLQueryItem(name: "per_page", value: "250"),
+ URLQueryItem(name: "page", value: "1"),
+ URLQueryItem(name: "sparkline", value: "true"),
+ URLQueryItem(name: "price_change_percentage", value: "24"),
+ ]
+ components.queryItems = components.queryItems.map {$0 + queryItems} ?? queryItems
+ 
+ var request = URLRequest(url: components.url!)
+ request.httpMethod = "GET"
+ request.timeoutInterval = 10
+ request.allHTTPHeaderFields = [
+ "accept": "application/json",
+ "x-cg-demo-api-key" : "CG-akbL7MnYLPmX7CAAnzEaUtyg"
+ ]
+ 
+ let (data, _) = try await URLSession.shared.data(for: request)
+ print(String(decoding: data, as: UTF8.self))
+ 
+ */
 
 import Foundation
 
-struct ExchangeModel {
+
+struct ExchangeModel: Identifiable, Codable {
+    let id, symbol, name: String
+    let image: String
+    let currentPrice: Double
+    let marketCap, marketCapRank, fullyDilutedValuation: Double?
+    let totalVolume, high24H, low24H: Double?
+    let priceChange24H, priceChangePercentage24H: Double?
+    let marketCapChange24H: Double?
+    let marketCapChangePercentage24H: Double?
+    let circulatingSupply, totalSupply, maxSupply, ath: Double?
+    let athChangePercentage: Double?
+    let athDate: String?
+    let atl, atlChangePercentage: Double?
+    let atlDate: String?
+    let lastUpdated: String?
+    let sparklineIn7D: SparklineIn7D?
+    let currentHoldings: Double?
     
+    enum CodingKeys: String, CodingKey {
+        case id, symbol, name, image
+        case currentPrice = "current_price"
+        case marketCap = "market_cap"
+        case marketCapRank = "market_cap_rank"
+        case fullyDilutedValuation = "fully_diluted_valuation"
+        case totalVolume = "total_volume"
+        case high24H = "high_24h"
+        case low24H = "low_24h"
+        case priceChange24H = "price_change_24h"
+        case priceChangePercentage24H = "price_change_percentage_24h"
+        case marketCapChange24H = "market_cap_change_24h"
+        case marketCapChangePercentage24H = "market_cap_change_percentage_24h"
+        case circulatingSupply = "circulating_supply"
+        case totalSupply = "total_supply"
+        case maxSupply = "max_supply"
+        case ath
+        case athChangePercentage = "ath_change_percentage"
+        case athDate = "ath_date"
+        case atl
+        case atlChangePercentage = "atl_change_percentage"
+        case atlDate = "atl_date"
+        case lastUpdated = "last_updated"
+        case sparklineIn7D = "sparkline_in_7d"
+        case currentHoldings
+    }
     
+    func updateHoldings(amount: Double) -> ExchangeModel {
+        return ExchangeModel(id: id,
+                             symbol: symbol,
+                             name: name,
+                             image: image,
+                             currentPrice: currentPrice,
+                             marketCap: marketCap,
+                             marketCapRank: marketCapRank,
+                             fullyDilutedValuation: fullyDilutedValuation,
+                             totalVolume: totalVolume,
+                             high24H: high24H,
+                             low24H: low24H,
+                             priceChange24H: priceChange24H,
+                             priceChangePercentage24H: priceChangePercentage24H,
+                             marketCapChange24H: marketCapChange24H,
+                             marketCapChangePercentage24H: marketCapChangePercentage24H,
+                             circulatingSupply: circulatingSupply,
+                             totalSupply: totalSupply,
+                             maxSupply: maxSupply,
+                             ath: ath,
+                             athChangePercentage: athChangePercentage,
+                             athDate: athDate,
+                             atl: atl,
+                             atlChangePercentage: atlChangePercentage,
+                             atlDate: atlDate,
+                             lastUpdated: lastUpdated,
+                             sparklineIn7D: sparklineIn7D,
+                             currentHoldings: amount)
+    }
     
+    var currentHoldingsValue: Double {
+        return (currentHoldings ?? 0) * currentPrice
+    }
+    
+    var rank: Int {
+        return Int(marketCapRank ?? 0)
+    }
+    
+}
+
+struct SparklineIn7D: Codable {
+    let price: [Double]?
 }
